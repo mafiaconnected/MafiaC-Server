d2b1f633dfc26f292409734719357c047746c77f
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-10-03 16:21:57 -0500

    Update bat files

--- Configure_VS2017_XP_x64.bat
+++ Configure_VS2017_XP_x64.bat
@@ -2,5 +2,6 @@
 set CMAKE_GENERATOR="Visual Studio 15 2017"
 set CMAKE_GENERATOR_PLATFORM=x64
 set CMAKE_GENERATOR_TOOLSET=v141_xp
+set CMAKE_EXTRA_ARGS=
 call Configure.bat
-pause
+if not defined AUTOMATION pause
